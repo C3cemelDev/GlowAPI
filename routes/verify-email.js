513729fc831df49773a530f980c062a67d9c49af
CecemelDev <cedricthauvoye1@gmail.com>
2022-01-06 13:00:05 +0100

    Update verify-email.js

--- routes/verify-email.js
+++ routes/verify-email.js
@@ -8,15 +8,16 @@ module.exports.Router = class Routes extends Router {
 
         this.get('/:token', async (req, res, next) => {
             try {
-                const { user: { id } } = jwt.verify(req.params.token, process.env.EMAIL_SECRET)
-                mysql.createQuery('UPDATE users SET isVerified=1 WHERE id=?', [id], (err, resu) => {
+                const { user} = jwt.verify(req.params.token, process.env.EMAIL_SECRET)
+                await mysql.createQuery('UPDATE users SET isVerified=1 WHERE id=?', [user.id], (err, resu) => {
                     if(err) res.send(err)
-                    res.send(resu)
+                    console.log(user.id)
+                    res.send(user.id)
                 })
             } catch (error) {
                 res.status(500)
             }
-            return res.redirect('/auth/login')
+            // return res.redirect('/auth/login')
         })
         
     }
